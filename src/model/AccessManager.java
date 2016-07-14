791e416312d8ff37f20f8c85b0d7704f6176bf0b
Satish <Satish@Lenovo-PC>
2016-07-13 20:52:22 -0400

    intial Comit

--- src/model/AccessManager.java
+++ src/model/AccessManager.java
@@ -1,22 +1,20 @@
 package model;
- 
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
- 
+
 import dao.Access;
 import dao.Database;
 import dto.Course;
- 
-public class AccessManager
-{
-public ArrayList<Course> getCourses() throws Exception
-{
-ArrayList<Course> courseList = new ArrayList<Course>();
-Database db = new Database();
-Connection con = db.getConnection();
-Access access = new Access();
-courseList = access.getCourses(con);
-return courseList;
-}
+
+public class AccessManager {
+	public ArrayList<Course> getCourses() throws Exception {
+		ArrayList<Course> courseList = new ArrayList<Course>();
+		Database db = new Database();
+		Connection con = db.getConnection();
+		Access access = new Access();
+		courseList = access.getCourses(con);
+		return courseList;
+	}
 }
